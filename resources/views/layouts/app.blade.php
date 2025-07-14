4e431644c0e6d3437697dba15861a2b25fb0f90e
anastianadewi <134130526+anastianadewi@users.noreply.github.com>
2025-07-14 20:13:41 +0700

    tambahan5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -196,7 +196,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                <button type="submit" class="btn btn-primary text-dark">Simpan</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
             </div>
         </form>
         </div>
